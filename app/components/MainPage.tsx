8267aec1336735fd1188379abd1a286895428078
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2024-11-27 09:25:35 +0530

    added the loading effect

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const MainPage = () => {
   return (
-    <div>
-        
+    <div id='ink-loading' className=''>
+        <p className='text-3xl'>Hello</p>
     </div>
   )
 }
